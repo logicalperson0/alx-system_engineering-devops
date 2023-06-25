f17274370f3350da6f207302dc2e6c1235211b91
Samuel <a2jsamuel@gmail.com>
2023-06-25 14:38:21 +0300

    Fix and add 100-file for task 4!

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,7 +1,11 @@
 #Changes SSH config file
-exec { 'echo':
-    ensure   => present,
-    path     => 'usr/bin:/bin',
-    command  => 'echo "     PasswordAuthentication no\n      ssh-copy-id -f ~/.ssh/school ubuntu@100.26.250.214" >> /etc/ssh/ssh_config',
-    provider => 'shell',
+file_line { 'passwd off and copy pub key':
+    ensure => present,
+    path   => '/etc/ssh/ssh_config',
+    line   => 'PasswordAuthentication no',
+    }
+file_line {'ssh copy id':
+    ensure => present.
+    path   => '/etc/ssh/ssh_config',
+    line   => 'ssh-copy-id -f ~/.ssh/school ubuntu@100.26.250.214',
     }
