2131717bc14e0f2cba2b5fb19289faaa8d861c2e
Samuel <a2jsamuel@gmail.com>
2023-06-25 14:29:33 +0300

    This is to fix 100-file and add it to fix task 4!

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -2,6 +2,6 @@
 exec { 'echo':
     ensure   => present,
     path     => 'usr/bin:/bin',
-    command  => 'echo "    PasswordAuthentication no         ssh-copy-id -f ~/.ssh/school ubuntu@100.26.250.214" >> /etc/ssh/ssh_config',
+    command  => 'echo "     PasswordAuthentication no\n      ssh-copy-id -f ~/.ssh/school ubuntu@100.26.250.214" >> /etc/ssh/ssh_config',
     provider => 'shell',
     }
