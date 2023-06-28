96a30932285cff26644add41603b4892e808e99f
Samuel <a2jsamuel@gmail.com>
2023-06-28 23:42:58 +0300

    This is for adv task 5!

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,24 @@
+# Installs a Nginx server and edit the default
+package {'nginx_webserver':
+    ensure      => installed,
+    name        => 'nginx',
+    description => 'a popular web server',
+    }
+
+file_line {'Add redirection permanently':
+    ensure => present,
+    path   => '/etc/nginx/sites-available/default',
+    line   => 'rewrite ^ /redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+    after  => 'listen 80 default_server;',
+    }
+
+file {'index.html':
+    ensure  => persent,
+    path    => '/var/www/html/index.html',
+    content => 'Hello World!',
+    }
+
+service {'restart nginx':
+    ensure => 'running',
+    name   => 'nginx',
+    }
