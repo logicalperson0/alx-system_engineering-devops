3baed8af21ce1c27f5ca0112b177f46131dced4f
Samuel <a2jsamuel@gmail.com>
2023-07-26 23:59:54 +0300

    Fix fir task 3!!!

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -8,36 +8,31 @@ import sys
 
 
 if __name__ == "__main__":
-    the_url = "https://jsonplaceholder.typicode.com/"
-    tasks_com = []
-    dicts_Id = {}
-    # dicts_all = {}
-    # tasks_all = []
+    the_url = 'https://jsonplaceholder.typicode.com/'
 
     user = '{}users'.format(the_url)
     requesting = requests.get(user)
     be_json = requesting.json()
-    # name = be_json['username']
+
+    dicts_all = {}
 
     for y in be_json:
-        name = y['username']
-        user_Id = y['id']
+        name = y.get('username')
+        user_Id = y.get('id')
 
         todos = '{}todos?userId={}'.format(the_url, user_Id)
-        requesting_todos = requests.get(todos)
-        be_jsonTodos = requesting_todos.json()
+        resquesting_Todos = requests.get(todos)
+        tasks = resquesting_Todos.json()
+        list_task = []
 
-        for x in be_jsonTodos:
-            dict_t = {"username": name, "task": x['title'],
-                      "completed": x['completed']}
-            tasks_com.append(dict_t)
-        # print(tasks_com)
+        for x in tasks:
+            dict_tasks = {"username": name,
+                         "task": x.get('title'),
+                         "completed": x.get('completed')}
+            list_task.append(dict_tasks)
 
-        dicts_Id[str(user_Id)] = tasks_com
+        dicts_all[str(user_Id)] = list_task
 
-        # tasks_all.append(dicts_Id)
-
-        # dicts_all[str(user_Id)] = tasks_all
-
-    with open("todo_all_employees.json", "w") as outfile:
-        json.dump(dicts_Id, outfile)
+    filename = 'todo_all_employees.json'
+    with open(filename, mode='w') as f:
+        json.dump(dicts_all, f)
