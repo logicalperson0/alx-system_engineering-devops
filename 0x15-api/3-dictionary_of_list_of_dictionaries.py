cf57edb701c3202462d5049cf3b4bf630b65c1c6
Samuel <a2jsamuel@gmail.com>
2023-07-26 23:48:53 +0300

    FIX  and add 3-file

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -6,7 +6,7 @@ import json
 import requests
 import sys
 
-"""
+
 if __name__ == "__main__":
     the_url = "https://jsonplaceholder.typicode.com/"
     tasks_com = []
@@ -39,29 +39,5 @@ if __name__ == "__main__":
 
         # dicts_all[str(user_Id)] = tasks_all
 
-        with open("todo_all_employees.json", "w") as outfile:
-            json.dump(dicts_Id, outfile)
-"""
-if __name__ == "__main__":
-    url = 'https://jsonplaceholder.typicode.com/'
-    user = '{}users'.format(url)
-    res = requests.get(user)
-    json_o = res.json()
-    d_task = {}
-    for user in json_o:
-        name = user.get('username')
-        userid = user.get('id')
-        todos = '{}todos?userId={}'.format(url, userid)
-        res = requests.get(todos)
-        tasks = res.json()
-        l_task = []
-        for task in tasks:
-            dict_task = {"username": name,
-                         "task": task.get('title'),
-                         "completed": task.get('completed')}
-            l_task.append(dict_task)
-
-        d_task[str(userid)] = l_task
-    filename = 'todo_all_employees.json'
-    with open(filename, mode='w') as f:
-        json.dump(d_task, f)
+    with open("todo_all_employees.json", "w") as outfile:
+        json.dump(dicts_Id, outfile)
