e6dd73e7eee1c8b9fa0d43e667398b1909015a09
Samuel <a2jsamuel@gmail.com>
2023-07-26 23:46:22 +0300

    TEST

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
-import csv
+"""
+Python script to export data in the JSON format.
+"""
 import json
 import requests
 import sys
-
-"""
-Python script to export data in the JSON format
-"""
 
 """
 if __name__ == "__main__":
