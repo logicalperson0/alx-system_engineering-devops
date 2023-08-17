c261624cfb6467f7a90932bfa9205accfbccbbc2
Samuel <a2jsamuel@gmail.com>
2023-08-18 01:12:23 +0300

    fix and add for 0-file

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -2,7 +2,7 @@
 
 exec {'no_limit':
   provider => shell,
-  command  => 'sudo sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 2048\"/" /etc/default/nginx'
+  command  => 'sudo sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/" /etc/default/nginx'
   before   => Exec['restart_nginx'],
 }
 
